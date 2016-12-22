63197c1318fa665a1147e1d170ce2270c9a2bf1b
njc24 <njc24@njit.edu>
2016-12-22 14:30:21 -0500

    gotta use the routes

--- nodejs-chicken-store/server.js
+++ nodejs-chicken-store/server.js
@@ -11,6 +11,8 @@ var app = express();
 // ./routes/index.js is the default code to be hit
 var appRoutes = require('./routes');
 
+app.use(appRoutes);
+
 // Begin listening on the configured port
 app.listen(config.port, () => {
   console.log('nodejs-chicken-store running on ' + process.env.APP_POD_NAME);
