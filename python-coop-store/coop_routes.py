e00376d9836bcdb3505fe11691693f27a9cb2da1
njc24 <njc24@njit.edu>
2016-12-29 13:10:44 -0500

    another attempt at Hello World

--- python-coop-store/coop_routes.py
+++ python-coop-store/coop_routes.py
@@ -1,11 +1,10 @@
-#!flask/bin/python
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
