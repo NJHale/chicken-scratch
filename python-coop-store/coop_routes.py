5f1a9933ee3e9d287902558b32b66ff67f7c934c
njc24 <njc24@njit.edu>
2016-12-29 13:00:20 -0500

    hello world maybe

--- python-coop-store/coop_routes.py
+++ python-coop-store/coop_routes.py
@@ -1,64 +1,11 @@
-# coop_routes.py
-import os
+#!flask/bin/python
 from flask import Flask
-from flask import jsonify
-from flask import request
-from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# # Get mongodb configurations
-# app.config['MONGO_DBNAME'] = os.getenv('MONGO_DBNAME', 'coopdb')
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
-#
-# mongo = PyMongo(app)
-#
-# @app.route('/star', methods=['GET'])
-# def get_all_stars():
-#   star = mongo.db.stars
-#   output = []
-#   for s in star.find():
-#     output.append({'name' : s['name'], 'distance' : s['distance']})
-#   return jsonify({'result' : output})
-#
-# @app.route('/star/', methods=['GET'])
-# def get_one_star(name):
-#   star = mongo.db.stars
-#   s = star.find_one({'name' : name})
-#   if s:
-#     output = {'name' : s['name'], 'distance' : s['distance']}
-#   else:
-#     output = "No such name"
-#   return jsonify({'result' : output})
-#
-# @app.route('/star', methods=['POST'])
-# def add_star():
-#   star = mongo.db.stars
-#   name = request.json['name']
-#   distance = request.json['distance']
-#   star_id = star.insert({'name': name, 'distance': distance})
-#   new_star = star.find_one({'_id': star_id })
-#   output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-#   return jsonify({'result' : output})
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
